c7021e5afd87a2ea1152953752297a5afaf13ef5
Max Baldwin <maxrbaldwin@gmail.com>
2016-08-11 18:13:50 -0500

    prompt coins complete

--- coins.js
+++ coins.js
@@ -1,18 +1,34 @@
-  var wallet = {
-    quarters: 0,
-    dimes: 0,
-    nickels: 0,
-    pennies: 0
-  };
+//a variable to prompt the user for a money value
+var moneyPrompt = prompt("Enter in a money amount (i.e. 0.00)");
 
-  function coinCounter () {
-  // Initialize a JavaScript object to hold the coins
+//start of the  function
+function coinCounter(moneyPrompt) {
+//oh hi, variable
   var coinPurse = {};
-
+//oh hi, coins
   coinPurse.quarters = 0;
-
+  coinPurse.dimes = 0;
+  coinPurse.nickels = 0;
+  coinPurse.pennies = 0;
+//quarter
+  coinPurse.quarters = Math.floor(moneyPrompt / 25);
+  moneyPrompt = moneyPrompt - (25 * coinPurse.quarters);
+//dimes
+  coinPurse.dimes = Math.floor(moneyPrompt / 10);
+  moneyPrompt = moneyPrompt - (10 * coinPurse.dimes);
+//nickels
+  coinPurse.nickels = Math.floor(moneyPrompt / 5);
+  moneyPrompt = moneyPrompt - (5 *coinPurse.nickels);
+//pennies
+  coinPurse.pennies = moneyPrompt - (5 * coinPurse.pennies);
+  moneyPrompt = moneyPrompt - (1 * coinPurse.pennies)
+  
   return coinPurse;
 }
 
-var coins = coinCounter()
-console.log();
+//Converts the moneyInput into a whole number
+moneyPrompt = moneyPrompt * 100;
+
+//put the stuff in the console
+var coins = coinCounter(moneyPrompt)
+console.log(coins);
