962f509314a55108573c2bed1b1869c9309a5ab9
Max Baldwin <maxrbaldwin@gmail.com>
2016-08-11 17:32:17 -0500

    adding function given on directions

--- coins.js
+++ coins.js
@@ -4,3 +4,15 @@
     nickels: 0,
     pennies: 0
   };
+
+  function coinCounter () {
+  // Initialize a JavaScript object to hold the coins
+  var coinPurse = {};
+
+  coinPurse.quarters = 0;
+
+  return coinPurse;
+}
+
+var coins = coinCounter()
+console.log();
